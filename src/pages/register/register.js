864d5949dac331b1322a5228b6a5530bbbb6b727
patrick.zyx <patrick.zyx@link8tech.tw>
2021-09-05 08:46:18 +0800

    register驗證

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,39 +1,97 @@
 import './register.scss';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
+import verify from './verify.js';
+// import alert from '../../common/components/alert/alert'
+import eye from '../../assets/icon_eye.png';
 
 const Register = () => {
-  const submitFuc = () => {
+  const [value, setValue] = useState({
+    account: '',
+    password: '',
+    repassword:'',
+  });
+  const [error, setError] = useState();
+  const [showText, setShowText] = useState(false);
 
+  const handleOnChange = (e) => {
+    setValue({
+      ...value,
+      [e.target.name]: e.target.value,
+    });
   };
+
+  const handleSubmit = (e) => {
+    setError(verify(value));
+  };
+
   return (
-      <div className='registerPage'>
-        <div className='registerForm'>
+      <div className="registerPage">
+        <div className="registerForm">
           <h1>註冊</h1>
-          <div className='formItem'>
-            <div className='formItem_title'>
+          <div className="formItem">
+            <div className="formItem_title">
               帳號
             </div>
-            <input className='formItem_input' type="text"></input>
-          </div>
-          <div className='formItem'>
-            <div className='formItem_title'>
-              密碼
-            </div>
-            <input className='formItem_input' type="text"></input>
-          </div>
-          <div className='formItem'>
-            <div className='formItem_title'>
-              確認密碼
-            </div>
-            <input className='formItem_input' type="text"></input>
-          </div>
-          <div className='verification'>
+
+            <input className={`formItem_input ${error?.account ?
+                'formItem_input-error' :
+                ''}`} type="text" name="account"
+                   onChange={handleOnChange}>
+            </input>
 
           </div>
-          <Link to="/"><span>返回登入</span></Link>
+          <p className="verifyText">
+            {error?.account && (<span>{error.account}</span>)}
+          </p>
+
+
+          <div className="formItem">
+            <div className="formItem_title">
+              密碼
+            </div>
+            <input className={`formItem_input ${error?.password ?
+                'formItem_input-error' :
+                ''}`}
+                   type={showText ? 'text' : 'password'} name="password"
+                   onChange={handleOnChange}>
+            </input>
+            <div className="formItem_eye" onClick={() => {
+              setShowText(!showText);
+            }}>
+              <img src={eye}></img>
+            </div>
+
+          </div>
+          <p className="verifyText">
+            {error?.password && (<span>{error.password}</span>)}
+          </p>
+
+          <div className="formItem">
+            <div className="formItem_title">
+              確認密碼
+            </div>
+            <input className={`formItem_input ${error?.repassword ?
+                'formItem_input-error' :
+                ''}`}
+                   type={showText ? 'text' : 'password'} name="repassword"
+                   onChange={handleOnChange}>
+            </input>
+            <div className="formItem_eye" onClick={() => {
+              setShowText(!showText);
+            }}>
+              <img src={eye}></img>
+            </div>
+
+          </div>
+          <p className="verifyText">
+            {error?.repassword && (<span>{error.repassword}</span>)}
+          </p>
+
+
+          <Link to="/"><span>登入</span></Link>
           <div>
-            <button onClick={submitFuc}>註冊</button>
+            <button onClick={handleSubmit}>登入</button>
           </div>
         </div>
 
---
+++ src/pages/register/verify.js
@@ -0,0 +1,39 @@
+const verify = (value) => {
+  let errorObj = {};
+
+  /*帳號
+  必須為信箱
+  */
+  if (!value.account) {
+    errorObj.account = '請輸入帳號';
+  }
+  const email = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  if(value.account && !email.test(value.account)){
+    errorObj.account = '信箱格式錯誤';
+  }
+
+  /*密碼
+  4-8字元；首尾必須是英文；中間必須是數字
+  */
+
+  if (!value.password) {
+    errorObj.password = '請輸入密碼';
+  }
+  // const passwordRules = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,10}/
+  // if(value.password && !email.test(value.account)){
+  //   errorObj.account = '密碼格式錯誤';
+  // }
+
+
+  /*密碼確認
+  與 "密碼相同"
+  */
+  if(value.repassword !== value.password){
+    errorObj.repassword = '與密碼不同';
+  }
+
+
+  return errorObj;
+};
+
+export default verify;
